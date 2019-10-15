75ac173d3ea2ab2e1b8feb00729999945061bca8
H4mid2019 <47919732+H4mid2019@users.noreply.github.com>
2019-10-15 22:36:59 +0300

    Add files via upload

---
+++ webscrapper.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+
+
+response = requests.get(
+    "https://stackoverflow.com/questions?tab=Newest", headers={'User-Agent': ua.chrome})
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+
+questions = soup.select(".question-summary")
+
+quest_dict = {}
+for question in questions:
+    quest = question.select_one(".question-hyperlink").getText()
+    vote = int(question.select_one(".vote-count-post").getText())
+    quest_dict[quest] = vote
+
+quest_dict_sorted = sorted(
+    quest_dict.items(), key=lambda kv: kv[1], reverse=True)
+
+for key, value in quest_dict_sorted:
+    print(key, "\t", value)
